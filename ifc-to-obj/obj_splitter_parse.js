a16266b50fe4e2348dbd2912cb93c4d116eac7ea
nmanzini <manzini.n@gmail.com>
2018-07-10 16:20:03 +0200

    compressing obj file

--- ifc-to-obj/obj_splitter_parse.js
+++ ifc-to-obj/obj_splitter_parse.js
@@ -1,10 +1,43 @@
 var fs = require('fs');
-let ifc_filepath = "ifc-to-obj/objs/15-34-IMB_MARCHE_optimisations-assimp.obj"
+let ifc_filepath = "objs/15-34-IMB_MARCHE_optimisations-assimp.obj"
 let fileContents = fs.readFileSync(ifc_filepath).toString('utf8')
 
 const OBJFile = require('obj-file-parser');
 const objFile = new OBJFile(fileContents);
-const output = objFile.parse()
+const original = objFile.parse();
 
-// outputs the first vertex
-console.log(output.models[0].faces[0]);
+const model = original.models[0];
+const faces = model.faces;
+const vertices = model.vertices;
+
+const vertices_num = vertices.length;
+const faces_num = faces.length;
+
+console.log(vertices_num, faces_num);
+
+function edit_get_ifc_sections(faces){
+    let sections = [];
+
+    faces.forEach((face,index,arr) => {
+        let group = face.group.split(/(?=[A-Z_])/)[1]
+        if (!sections.includes(group)){
+            sections.push(group);
+        }
+    });
+    return (sections);
+}
+
+const ifc_sections = edit_get_ifc_sections(faces);
+
+console.log(ifc_sections);
+
+faces.forEach((face,index,arr) => {
+    let group = face.group.split(/(?=[A-Z_])/)[1]
+    if (!sections.includes(group)){
+        sections.push(group);
+    }
+});
+
+// original.models[0].vertices.forEach((value,index,arr) => {
+//     console.log(index)
+// });
