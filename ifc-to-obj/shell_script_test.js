e5f312a809ed90dd9c0bc61cfbf141145cdb8c35
nmanzini <manzini.n@gmail.com>
2018-07-11 18:25:42 +0200

    nodejs converter using assimp

---
+++ ifc-to-obj/shell_script_test.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+
+const model = "/Users/nicola/code/clovis-3d/ifc-to-obj/ifcs/15"
+const out = "/Users/nicola/code/clovis-3d/ifc-to-obj/objs/15.obj"
+const h = "obj"
+
+const assimp = spawn('assimp', ['export',model,out,h]);
+
+assimp.stdout.on('data', (data) => {
+	console.log(` stdout:\n${data}`);
+});
+
+assimp.stderr.on('data', (data) => {
+	console.log(` stderr:\n${data}`);
+});
+
+assimp.on('close', (code) => {
+	console.log(` close: child process exited with code ${code}`);
+});
