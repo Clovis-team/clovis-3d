262938cda3c2f9ccd36dc4d54646ac3bbe927f6c
nmanzini <manzini.n@gmail.com>
2018-07-17 01:42:35 +0200

    starting dev threejs - mouse interaction

--- threejs/three-viz-gltf.js
+++ threejs/three-viz-gltf.js
@@ -1,14 +1,18 @@
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xaaaabb );
+window.scene = scene
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var controls = new THREE.OrbitControls( camera );
+var mouse = new THREE.Vector2();
+var INTERSECTED
 
+var controls = new THREE.OrbitControls( camera );
 controls.enableDamping = true;
 controls.screenSpacePanning = true;
 controls.panSpeed = 0.3;
@@ -18,8 +22,6 @@ controls.target.set(80, 0, 20);
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
-console.log(directionalLight);
-directionalLight.position= THREE.Vector3(0,-10,10);
 scene.add( directionalLight );
 
 
@@ -27,6 +29,7 @@ var loader = new THREE.GLTFLoader();
 
 var building_main = {}
 var floors = {}
+var objects_flat = [];
 
 var explosion = {
     z_old:0,
@@ -51,9 +54,9 @@ loader.load(
         building_main = (gltf.scene.children[0])
         scene.add(building_main);
         floors = scene.children[2].children[0].children[0].children[0].children
-        console.log(floors);
         populate_gui_floors(floors);
-
+        objects_flat = flatten(floors);
+        // console.log(objects_flat);
     },
     // called while loading is progressing
     function ( xhr ) {
@@ -74,7 +77,6 @@ let gui_floor_folder = gui.addFolder('Floors');
 
 function populate_gui_floors(floors){
     for (i = floors.length - 1; i>=0; i--){
-        console.log(i,floors[i]);
         let floor_name = floors[i].name.split('_')[1];
         gui_floor_folder.add(floors[i], "visible").name(floor_name);
     }
@@ -99,7 +101,12 @@ function explode_floors(floors){
 
 gui.add(explosion,"z_new",0,100).name("z_explosion");
 
-controls.update();
+raycaster = new THREE.Raycaster();
+
+// stats = new Stats();
+// container.appendChild( stats.dom );
+document.addEventListener( 'mouseup', onDocumentMouseMove, false );
+window.addEventListener( 'resize', onWindowResize, false );
 
 function animate() {
     requestAnimationFrame( animate );
@@ -111,8 +118,51 @@ function render(){
     if (floors.length){
         explode_floors(floors)
     };
+
+    if (mouse.updated){
+        console.log("click")
+        raycaster.setFromCamera( mouse, camera );
+        var intersects = raycaster.intersectObjects( objects_flat );
+        if ( intersects.length > 0 ) {
+            INTERSECTED = intersects[0].object;
+            console.log(intersects);
+
+            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            INTERSECTED.material = material;
+        }
+        mouse.updated = false;
+    };
+
     // required if controls.enableDamping or controls.autoRotate are set to true
     controls.update();
     renderer.render( scene, camera );
 }
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onDocumentMouseMove( event ) {
+    event.preventDefault();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.updated = true;
+}
+
+function flatten(items) {
+    const flat = [];
+  
+    items.forEach(item => {
+      if (Array.isArray(item.children)) {
+        flat.push(...item.children);
+      };
+    });
+  
+    return flat;
+  }
+
+controls.update();
+
 animate();
