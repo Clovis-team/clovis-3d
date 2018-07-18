a5934a736cd0a84e0c2b3e9444a578b3c388f746
nicola <manzini.n@gmail.com>
2018-07-18 19:46:21 +0200

    added persp/orto switch, added ui element infos

--- threejs/three-viz-gltf.js
+++ threejs/three-viz-gltf.js
@@ -1,23 +1,91 @@
-var scene = new THREE.Scene();
+/**
+ * @author nmanzini / https://www.nicolamanzini.com/
+ */
+
+
+var scene, camera, control, renderer
+
+scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xaaaabb );
 window.scene = scene
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// console.log(camera);
 
-var renderer = new THREE.WebGLRenderer();
+// camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// var ratio = window.innerWidth/window.innerHeight;
+// var height = 20
+// var width = 20 * ratio;
+// var camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+
+var camera_types = ["Perspective","Ortographic","Walking"]
+
+// scene.add( camera );
+
+renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var mouse = new THREE.Vector2();
-var INTERSECTED
+var INTERSECTED = {}
 
-var controls = new THREE.OrbitControls( camera );
-controls.enableDamping = true;
-controls.screenSpacePanning = true;
-controls.panSpeed = 0.3;
-controls.rotateSpeed = 0.2;
-controls.target.set(80, 0, 20);
+// var controls = new THREE.OrbitControls(camera,renderer.domElement);
+// controls.enableDamping = true;
+// controls.screenSpacePanning = true;
+// controls.panSpeed = 0.3;
+// controls.rotateSpeed = 0.2;
+// controls.target.set(80, 0, 20);
+
+
+setup_camera(camera_types[0],camera);
+
+function setup_camera(type,old_camera){
+    let new_camera;
+    if (type == camera_types[0]){
+        new_camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        if (typeof old_camera != "undefined") {
+            new_camera.position.copy(old_camera.position);
+            new_camera.rotation.copy(old_camera.rotation);
+        }
+    } else if (type == camera_types[1]){
+        let ratio = window.innerWidth/window.innerHeight;
+        let height = 100
+        let width = height * ratio;
+        new_camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, -1000, 1000 );
+        if (typeof old_camera != "undefined") {
+            new_camera.position.copy(old_camera.position);
+            new_camera.rotation.copy(old_camera.rotation);
+        }
+    } else{
+        console.log("camera not recognized")
+    };
+    camera = new_camera;
+
+    let new_controls = new THREE.OrbitControls(new_camera,renderer.domElement);
+    if (typeof controls != "undefined") {
+        new_controls.target.copy(controls.target);
+    }else{
+        new_controls.target.set(80, 0, 20);
+    }
+    new_controls.enableDamping = true;
+    new_controls.screenSpacePanning = true;
+    new_controls.panSpeed = 0.3;
+    new_controls.rotateSpeed = 0.2;
+    
+    controls = new_controls;
+    console.log(controls);
+    console.log(camera)
+    
+}
+
+// camera.position.set(80, 0, 20);
+// var controls = new THREE.FirstPersonControls(camera);
+// camera.constrainVertical = true;
+// controls.lookSpeed = 0.5;
+// controls.movementSpeed = 10;
+
+
 
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
@@ -28,29 +96,40 @@ var stats = new Stats();
 stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
 
+
+
 var gui = new dat.GUI();
-let gui_floor_folder = gui.addFolder('Floors');
-var explosion = {
-    z_old:0,
-    z_new:0,
-    z_delta:0
-};
-gui.add(explosion,"z_new",0,100).name("z_explosion");
-let gui_ifc_tags_folder = gui.addFolder('Ifc Tags');
+// creating gui folders
+
+
+let gui_camera = gui.addFolder('Camera options');
+let new_camera= {type : camera_types[0]}
+let controller = gui_camera.add(new_camera, 'type', camera_types);
+controller.onChange(function(value) {
+    setup_camera(value,camera);
+    }
+);
+
+
+
+
+
+let gui_intersected =  {ifc_tag:"none",ifc_name:"none"}
+gui.add(gui_intersected,"ifc_tag").listen();
+gui.add(gui_intersected,"ifc_name").listen();
+
+console.log(gui);
+
 
 var building_main = {}
 var floors = {}
 let ifc_building_elements = [];
-
 // ifc_building_elements is an array of objects3d
 // each object has as child all the meshes and objs
 //  of a certain building element (wall, slab...)
 var mesh_all = [];
 
-
-
 var loader = new THREE.GLTFLoader();
-
 
 var t0 = performance.now();
 // Load a glTF resource
@@ -73,8 +152,10 @@ loader.load(
         building = (gltf.scene.children[0].children[0].children[0].children[0]);
         floors = building.children;
         populate_gui_floors(floors);
-        ifc_building_elements = get_building_elements(scene);
+        
+        ifc_building_elements = get_building_elements(building);
         populate_gui_ifc_tags(ifc_building_elements);
+        populate_gui_explosion(floors);
         var t2 = performance.now();
         console.log("load and name all groups " + Math.round(t2 - t1) + " milliseconds.")
         console.log(ifc_building_elements)
@@ -91,7 +172,6 @@ loader.load(
 
 function get_building_elements(scene){
     let ifc_building_elements = []
-    ifc_building_elements.exist = true;
     scene.traverse( function( node ) {
         if ( (node instanceof THREE.Mesh || node instanceof THREE.Object3D) && node.name != "") {
             mesh_all.push(node);
@@ -99,7 +179,7 @@ function get_building_elements(scene){
             node.ifc_tag = ifc_tag;
             let ifc_name = node.name.split('_')[1];
             node.ifc_name = ifc_name;
-            if ( ifc_tag != "" && ifc_tag.charAt(0) != "$" && ifc_tag != "mesh")
+            if ( ifc_tag != "" && ifc_tag.charAt(0) != "$" && ifc_tag != "mesh" && ifc_tag != "IfcBuildingStorey")
             {
                 if ( !ifc_building_elements.some(obj => obj.ifc_tag === ifc_tag
                     )){
@@ -122,6 +202,7 @@ function get_building_elements(scene){
 }
 
 function populate_gui_floors(floors){
+    let gui_floor_folder = gui.addFolder('Floors');
     for (i = floors.length - 1; i>=0; i--){
         let floor_name = floors[i].name.split('_')[1];
         gui_floor_folder.add(floors[i], "visible").name(floor_name);
@@ -129,15 +210,39 @@ function populate_gui_floors(floors){
 }
 
 function populate_gui_ifc_tags(elements){
+    let gui_ifc_tags_folder = gui.addFolder('Ifc Tags');
     elements.forEach(element => {
-        let ifc_tag = element.name;
-        gui_ifc_tags_folder.add(element, "visible_order").name(ifc_tag);
+        let ifc_tag = element.name
+        let controller =  gui_ifc_tags_folder.add(element, "visible_order").name(ifc_tag);
 
+        controller.onChange(function(value) {
+            if (element.visible_order != element.visible ){
+                element.children.forEach(obj =>{
+                    obj.visible = element.visible_order;
+                })
+                element.visible = element.visible_order;
+            }
+        });
     })
-    // for (i = elements.length - 1; i>=0; i--){
-    //     let ifc_tag = elements[i].name;
-    //     gui_ifc_tags_folder.add(elements[i], "visible_order").name(ifc_tag);
-    // }
+}
+
+function populate_gui_explosion(floors){
+    var explosion = {
+        z_old:0,
+        z_new:0,
+        z_delta:0
+    };
+
+    let controller =  gui.add(explosion,"z_new",0,100).name("z_explosion");
+
+    controller.onChange( value => {
+        explosion.z_delta = explosion.z_new -explosion.z_old;
+        floors.forEach((floor,index) => {
+            floor.position.z += (explosion.z_delta * index);
+        });
+        explosion.z_old = explosion.z_new;
+        explosion.z_delta=0;
+    })
 }
 
 function explode_floors(floors){
@@ -154,60 +259,10 @@ function explode_floors(floors){
     explosion.z_delta=0;
 }
 
-
-
 raycaster = new THREE.Raycaster();
 
 document.addEventListener( 'mouseup', onDocumentMouseMove, false );
 window.addEventListener( 'resize', onWindowResize, false );
-
-function animate() {
-    requestAnimationFrame( animate );
-
-    stats.begin();
-
-    controls.update();
-    render()
-
-	stats.end();
-};
-
-function render(){
-    if (floors.length){
-        explode_floors(floors)
-    };
-
-    if (mouse.updated){
-        console.log("click")
-        raycaster.setFromCamera( mouse, camera );
-        var intersects = raycaster.intersectObjects( mesh_all );
-        if ( intersects.length > 0 ) {
-            INTERSECTED = intersects[0].object;
-            console.log("OBJ", INTERSECTED)
-            console.log("PAR 1", INTERSECTED.parent)
-            console.log("PAR 2", INTERSECTED.parent.parent)
-            console.log("PAR 3", INTERSECTED.parent.parent.parent)
-            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            INTERSECTED.material = material;
-        }
-        mouse.updated = false;
-    };
-
-    if (ifc_building_elements.exist){
-        ifc_building_elements.forEach(element => {
-            if (element.visible_order != element.visible ){
-                element.children.forEach(obj =>{
-                    obj.visible = element.visible_order;
-                })
-                element.visible = element.visible_order;
-            }
-        })
-    }
-
-    // required if controls.enableDamping or controls.autoRotate are set to true
-    controls.update();
-    renderer.render( scene, camera );
-}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -222,18 +277,40 @@ function onDocumentMouseMove( event ) {
     mouse.updated = true;
 }
 
-function flatten(items) {
-    const flat = [];
-  
-    items.forEach(item => {
-      if (Array.isArray(item.children)) {
-        flat.push(...item.children);
-      };
-    });
-  
-    return flat;
-  }
+// const clock = new THREE.Clock( true );
 
-// controls.update();
+function animate() {
+    requestAnimationFrame( animate );
+
+    stats.begin();
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    // controls.update( clock.getDelta() );
+    controls.update();
+
+    render()
+
+	stats.end();
+};
+
+function render(){
+
+    if (mouse.updated){
+        raycaster.setFromCamera( mouse, camera );
+        var intersects = raycaster.intersectObjects( mesh_all );
+        if ( intersects.length > 0 ) {
+            INTERSECTED = intersects[0].object;
+            gui_intersected.ifc_tag = INTERSECTED.ifc_tag
+            gui_intersected.ifc_name = INTERSECTED.ifc_name
+            console.log("OBJ", INTERSECTED)
+            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            INTERSECTED.material = material;
+        }
+        mouse.updated = false;
+    };
+
+    renderer.render( scene, camera );
+}
+
 
 animate();
