42e3212a5af3a326427c4f9bff8df1c9e4395c95
nicola <manzini.n@gmail.com>
2018-07-19 16:44:14 +0200

    reding folder

--- ifc-to-obj/shell_script_test.js
+++ ifc-to-obj/shell_script_test.js
@@ -1,5 +1,14 @@
 const { spawn } = require('child_process');
 
+const ifcs = './ifcs/';
+const fs = require('fs');
+
+// fs.readdir(ifcs, (err, files) => {
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// })
+
 const model = "/Users/nicola/code/clovis-3d/ifc-to-obj/ifcs/15"
 const out = "/Users/nicola/code/clovis-3d/ifc-to-obj/objs/15.obj"
 const h = "obj"
