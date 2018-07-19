ed89aad44f10a7f89fea03c8b1140ec4296c4647
nicola <manzini.n@gmail.com>
2018-07-19 16:45:25 +0200

    centering camera on any file

--- threejs/three-viz-gltf.js
+++ threejs/three-viz-gltf.js
@@ -3,6 +3,13 @@
  */
 
 var scene, camera, control, renderer
+var gui, stats
+
+var gui_intersected
+var loader = new THREE.GLTFLoader();
+
+gui = new dat.GUI();
+// creating gui folders
 
 scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xaaaabb );
@@ -14,6 +21,15 @@ renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
+scene.add( directionalLight );
+
+stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
 
 var mouse = new THREE.Vector2();
 var INTERSECTED = {}
@@ -41,8 +57,6 @@ function setup_camera(type,old_camera){
         console.log("camera not recognized")
     };
     camera = new_camera;
-    console.log(camera)
-
     let new_controls = new THREE.OrbitControls(camera,renderer.domElement);
     if (typeof controls != "undefined") {
         new_controls.target.copy(controls.target);
@@ -55,54 +69,8 @@ function setup_camera(type,old_camera){
     new_controls.rotateSpeed = 0.2;
     new_controls.screenSpacePanning = true;
 
-    
     controls = new_controls;
-    console.log(controls);
-    
-    
 }
-
-// camera.position.set(80, 0, 20);
-// var controls = new THREE.FirstPersonControls(camera);
-// camera.constrainVertical = true;
-// controls.lookSpeed = 0.5;
-// controls.movementSpeed = 10;
-
-
-
-var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
-scene.add( directionalLight );
-
-var stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
-
-
-
-var gui = new dat.GUI();
-// creating gui folders
-
-
-let gui_camera = gui.addFolder('Camera options');
-let new_camera= {type : camera_types[0]}
-let controller = gui_camera.add(new_camera, 'type', camera_types);
-controller.onChange(function(value) {
-    setup_camera(value,camera);
-    }
-);
-
-
-
-
-
-let gui_intersected =  {ifc_tag:"none",ifc_name:"none"}
-gui.add(gui_intersected,"ifc_tag").listen();
-gui.add(gui_intersected,"ifc_name").listen();
-
-console.log(gui);
-
 
 var building_main = {}
 var floors = {}
@@ -112,46 +80,98 @@ let ifc_building_elements = [];
 //  of a certain building element (wall, slab...)
 var mesh_all = [];
 
-var loader = new THREE.GLTFLoader();
-
 var t0 = performance.now();
-// Load a glTF resource
-loader.load(
-    // resource URL
-    'gltfs/15-assimp.gltf',
-    // 'gltfs/Project1-assimp.gltf',
-    // called when the resource is loaded
-    function ( gltf ) {
-        // scene.add( gltf.scene );
-        // gltf.animations; // Array<THREE.AnimationClip>
-        // gltf.scene; // THREE.Scene   
-        // gltf.scenes; // Array<THREE.Scene>
-        // gltf.cameras; // Array<THREE.Camera>
-        // gltf.asset; // Object
-        // var t0 = performance.now();
-        var t1 = performance.now();
-        console.log("load gltf took " + Math.round(t1 - t0) + " milliseconds.")
-        scene.add(gltf.scene);
-        building = (gltf.scene.children[0].children[0].children[0].children[0]);
-        floors = building.children;
-        populate_gui_floors(floors);
-        
-        ifc_building_elements = get_building_elements(building);
-        populate_gui_ifc_tags(ifc_building_elements);
-        populate_gui_explosion(floors);
-        var t2 = performance.now();
-        console.log("load and name all groups " + Math.round(t2 - t1) + " milliseconds.")
-        console.log(ifc_building_elements)
-    },
-    // called while loading is progressing
-    function ( xhr ) {
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    },
-    // called when loading has errors
-    // function ( error ) {
-    //     console.log( 'An error happened' );
-    // }
-);
+
+
+
+populate_gui_camera();
+// load_gltf_file('gltfs/Project1-assimp.gltf')
+load_gltf_file('gltfs/15-assimp.gltf')
+function load_gltf_file(URL){
+    // Load a glTF resource
+    loader.load(
+        // resource URL
+        URL,
+    
+        // called when the resource is loaded
+        function ( gltf ) {
+            // scene.add( gltf.scene );
+            // gltf.animations; // Array<THREE.AnimationClip>
+            // gltf.scene; // THREE.Scene   
+            // gltf.scenes; // Array<THREE.Scene>
+            // gltf.cameras; // Array<THREE.Camera>
+            // gltf.asset; // Object
+            var t1 = performance.now();
+            console.log("load gltf took " + Math.round(t1 - t0) + " milliseconds.")
+            
+            scene.add(gltf.scene);
+            center_and_position_camera(scene,camera,controls)
+            
+            building = (gltf.scene.children[0].children[0].children[0].children[0]);
+            floors = building.children;
+            
+            populate_gui_floors(floors);
+            ifc_building_elements = get_building_elements(building);
+            populate_gui_ifc_tags(ifc_building_elements);
+            populate_gui_explosion(floors);
+            populate_ifc_tag_gui();
+            var t2 = performance.now();
+            console.log("load and name all groups " + Math.round(t2 - t1) + " milliseconds.")
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        // called when loading has errors
+        // function ( error ) {
+        //     console.log( 'An error happened' );
+        // }
+    );
+}
+
+function center_and_position_camera(object, camera, controls){
+    object.updateMatrixWorld();
+    const box = new THREE.Box3().setFromObject(object);
+    var size = new THREE.Vector3
+    box.getSize(size)
+    var center = new THREE.Vector3
+    box.getCenter(center);
+    controls.target.copy(center);
+    camera.position.copy(center.add(size))
+}
+
+function get_main_floor(floors){
+    let max_value = 0;
+    let max_id = 0;
+    floors.forEach((floor,index) =>{
+        if (floor.children.length > max_value){
+            max_value = floor.children.length;
+            max_id = index;
+        }
+    });
+    return max_id
+}
+
+function find_center(object3D){
+    let centers = []
+    let length = object3D.children.length
+    object3D.children.forEach(obj => {
+        centers.push( obj.position)
+    })
+    let x = 0
+    let y = 0
+    let z = 0
+    centers.forEach(vector =>{
+        x+=vector.x
+        y+=vector.y
+        z+=vector.z
+    })
+    x /= length;
+    y /= length;
+    z /= length;
+    center = new THREE.Vector3(x,y,z);
+    return center
+}
 
 function get_building_elements(scene){
     let ifc_building_elements = []
@@ -228,6 +248,21 @@ function populate_gui_explosion(floors){
     })
 }
 
+function populate_gui_camera(){
+    let gui_camera = gui.addFolder('Camera options');
+    let new_camera= {type : camera_types[0]}
+    let controller = gui_camera.add(new_camera, 'type', camera_types);
+    controller.onChange(function(value){
+        setup_camera(value,camera);
+    });
+};
+
+function populate_ifc_tag_gui(){
+    gui_intersected =  {ifc_tag:"none",ifc_name:"none"}
+    gui.add(gui_intersected,"ifc_tag").listen();
+    gui.add(gui_intersected,"ifc_name").listen();
+}
+
 function explode_floors(floors){
     explosion.z_delta = explosion.z_new -explosion.z_old
     // TODO: find the floor with most stuff and center the explosi
@@ -246,6 +281,7 @@ raycaster = new THREE.Raycaster();
 
 document.addEventListener( 'click', onDocumentMouseMove, false );
 window.addEventListener( 'resize', onWindowResize, false );
+
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
