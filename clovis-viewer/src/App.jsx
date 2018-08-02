451db4ca97c67e3c15a96ce9ae5898b21b28c301
Clément Fradet Normand <clement.fradet.normand@gmail.com>
2018-08-02 14:10:15 +0200

    add first css to work on it

--- clovis-viewer/src/App.jsx
+++ clovis-viewer/src/App.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PopupViewer from './components/PopupViewer';
 import './App.css';
+import logo from './assets/logo/clovis-white.svg'
+import { throws } from 'assert';
 
-const App = () => (
-
-  <div className="App">
-
-    Hello Clovis App
+class App extends Component {
 
 
-    <PopupViewer />
+  constructor() {
+    super()
+    this.state = {
+      isViewerOpened: false,
+    }
+  }
 
-  </div>
+  OnViewerOpen = () => {
+    this.setState({
+      isViewerOpened: true,
+    })
+  }
 
-);
+  OnViewerClose = () => {
+    this.setState({
+      isViewerOpened: false,
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <div 
+          className="viewer-button"
+          onClick={(e) => this.OnViewerOpen()}
+        >
+          <img 
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <p>
+            Open Clovis viewer
+          </p>
+        </div>
+
+        <PopupViewer 
+          isViewerOpened={this.state.isViewerOpened}
+          OnViewerClose={this.state.OnViewerClose}
+        />
+      </div>
+    )
+  }
+
+
+};
 
 export default App;
+
+
--- clovis-viewer/src/index.js
+++ clovis-viewer/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
   React.createElement(App),
