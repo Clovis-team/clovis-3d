45d7f29ea07999510db53194f5c45e73ce025f11
nicola <manzini.n@gmail.com>
2018-08-07 15:03:17 +0200

    cleanup canvas

--- clovis-viewer/src/components/ThreeContainer/threejs/SceneManager.js
+++ clovis-viewer/src/components/ThreeContainer/threejs/SceneManager.js
@@ -2,41 +2,43 @@ import 'three/examples/js/controls/OrbitControls';
 import SceneSubject from './SceneSubject';
 import GeneralLights from './GeneralLights';
 
-export default canvas => {
-    console.log(THREE.OrbitControls)
+export default (canvas) => {
+    console.log(THREE.OrbitControls);
     const clock = new THREE.Clock();
-    const origin = new THREE.Vector3(0,0,0);
+    const origin = new THREE.Vector3(0, 0, 0);
 
     const screenDimensions = {
         width: canvas.width,
-        height: canvas.height
-    }
+        height: canvas.height,
+    };
 
     const mousePosition = {
         x: 0,
-        y: 0
-    }
+        y: 0,
+
+    };
 
     const scene = buildScene();
     const renderer = buildRender(screenDimensions);
     const camera = buildCamera(screenDimensions);
+    const controls = buildControls(camera, renderer);
     const sceneSubjects = createSceneSubjects(scene);
 
     function buildScene() {
         const scene = new THREE.Scene();
-        scene.background = new THREE.Color("#FFF");
+        scene.background = new THREE.Color('#FFF');
 
         return scene;
     }
 
     function buildRender({ width, height }) {
-        const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true }); 
+        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
         const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
         renderer.setPixelRatio(DPR);
         renderer.setSize(width, height);
 
         renderer.gammaInput = true;
-        renderer.gammaOutput = true; 
+        renderer.gammaOutput = true;
 
         return renderer;
     }
@@ -45,18 +47,22 @@ export default canvas => {
         const aspectRatio = width / height;
         const fieldOfView = 60;
         const nearPlane = 4;
-        const farPlane = 100; 
+        const farPlane = 100;
         const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
 
         camera.position.z = 40;
 
         return camera;
     }
+    function buildControls(camera, renderer) {
+        const controls = new THREE.OrbitControls(camera, renderer.domElement);
+        return controls;
+    }
 
     function createSceneSubjects(scene) {
         const sceneSubjects = [
             new GeneralLights(scene),
-            new SceneSubject(scene)
+            new SceneSubject(scene),
         ];
 
         return sceneSubjects;
@@ -65,29 +71,28 @@ export default canvas => {
     function update() {
         const elapsedTime = clock.getElapsedTime();
 
-        for(let i=0; i<sceneSubjects.length; i++)
-            sceneSubjects[i].update(elapsedTime);
+        for (let i = 0; i < sceneSubjects.length; i++) { sceneSubjects[i].update(elapsedTime); }
 
-        updateCameraPositionRelativeToMouse();
-
+        // updateCameraPositionRelativeToMouse();
+        controls.update();
         renderer.render(scene, camera);
     }
 
     function updateCameraPositionRelativeToMouse() {
-        camera.position.x += (  (mousePosition.x * 0.01) - camera.position.x ) * 0.01;
-        camera.position.y += ( -(mousePosition.y * 0.01) - camera.position.y ) * 0.01;
+        camera.position.x += ((mousePosition.x * 0.01) - camera.position.x) * 0.01;
+        camera.position.y += (-(mousePosition.y * 0.01) - camera.position.y) * 0.01;
         camera.lookAt(origin);
     }
 
     function onWindowResize() {
         const { width, height } = canvas;
-        
+
         screenDimensions.width = width;
         screenDimensions.height = height;
 
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
-        
+
         renderer.setSize(width, height);
     }
 
@@ -99,6 +104,6 @@ export default canvas => {
     return {
         update,
         onWindowResize,
-        onMouseMove
-    }
-}
+        onMouseMove,
+    };
+};
