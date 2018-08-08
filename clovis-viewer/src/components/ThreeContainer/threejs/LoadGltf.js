48266dd3b1476033e3c47129db8d166155c77f1b
nicola <manzini.n@gmail.com>
2018-08-08 17:56:23 +0200

    dipatching loaded event

--- clovis-viewer/src/components/ThreeContainer/threejs/LoadGltf.js
+++ clovis-viewer/src/components/ThreeContainer/threejs/LoadGltf.js
@@ -3,8 +3,8 @@ import 'three/examples/js/loaders/GLTFLoader';
 
 export default (scene, buildingPath) => {
     const loader = new THREE.GLTFLoader();
-    let building;
-    let floors;
+    let gltfBuilding;
+    let gltfFloors;
 
     loader.load(
         // resource URL
@@ -13,10 +13,12 @@ export default (scene, buildingPath) => {
         // called when the resource is loaded
         (gltf) => {
             scene.add(gltf.scene);
-            console.log('building loaded');
-            // sorry for this bad code!
-            building = gltf.scene.children[0].children[0].children[0].children[0];
-            floors = building.children;
+            console.log('building loaded, event "loadedGltf" emitted');
+
+            dispatchLoadedEvent();
+
+            gltfBuilding = gltf.scene.children[0].children[0].children[0].children[0];
+            gltfFloors = building.children;
         },
         // called while loading is progressing
         (xhr) => {
@@ -29,13 +31,41 @@ export default (scene, buildingPath) => {
         },
     );
 
-
-    function update() {
+    function getObjectCenter(object) {
+        const box = new THREE.Box3().setFromObject(object);
+        const centerVector = new THREE.Vector3();
+        box.getCenter(centerVector);
+        return centerVector;
     }
 
+    function getgetObjectSize(object) {
+        const box = new THREE.Box3().setFromObject(object);
+        const sizeVector = new THREE.Vector3();
+        box.getSize(sizeVector);
+        return sizeVector;
+    }
+
+    function dispatchLoadedEvent() {
+        const event = new Event('loadedGltf');
+        const center = getObjectCenter(scene);
+        const size = getgetObjectSize(scene);
+        const cameraPosition = getObjectCenter(scene).add(getgetObjectSize(scene).divideScalar(2));
+        event.center = (center);
+        event.size = (size);
+        event.cameraPosition = (cameraPosition);
+        window.dispatchEvent(event);
+    }
+
+    // TODO: ask mathias if there is a better way to export values
+    function building() {
+        return gltfBuilding;
+    }
+
+    function floors() {
+        return gltfFloors;
+    }
+
+
     return {
-        update,
-        building,
-        floors,
     };
 };
