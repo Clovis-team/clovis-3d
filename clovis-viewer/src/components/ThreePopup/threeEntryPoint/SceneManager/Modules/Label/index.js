c2f3e6ba59fd794549622bfe27a176e84d808165
Clément Fradet Normand <clement.fradet.normand@gmail.com>
2018-08-29 09:18:33 +0200

    little fix of label selection

--- clovis-viewer/src/components/ThreePopup/threeEntryPoint/SceneManager/Modules/Label/index.js
+++ clovis-viewer/src/components/ThreePopup/threeEntryPoint/SceneManager/Modules/Label/index.js
@@ -6,7 +6,7 @@ function Label({
     const tags = [];
     labels.forEach((label) => {
         const tag = createDiv(label);
-        tag.addEventListener('click', (event) => {
+        tag.addEventListener('mousedown', (event) => {
             event.stopImmediatePropagation();
             event.preventDefault();
             event.stopPropagation();
