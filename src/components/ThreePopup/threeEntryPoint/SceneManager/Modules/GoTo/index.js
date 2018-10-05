6dc57f5812b6f22c6f99e53b24d4972afe1edbf5
nicola <manzini.n@gmail.com>
2018-10-05 15:55:16 -0500

    function fully works

--- src/components/ThreePopup/threeEntryPoint/SceneManager/Modules/GoTo/index.js
+++ src/components/ThreePopup/threeEntryPoint/SceneManager/Modules/GoTo/index.js
@@ -1,5 +1,5 @@
 function GoTo({ camera, controls }) {
-    this.movementSpeed = 10; // units per second
+    this.movementSpeed = 5; // units per second
 
     this.running = false;
 
@@ -12,28 +12,56 @@ function GoTo({ camera, controls }) {
     const controlsDir = new THREE.Vector3();
     const controlsDest = new THREE.Vector3();
 
+    let camSpeed;
+    let conSpeed;
+
     this.start = (caDest, coDest) => {
         cameraDest.copy(caDest);
         controlsDest.copy(coDest);
 
-        cameraDir.subVectors(cameraDest, camera.position).normalize();
-        controlsDir.subVectors(controlsDest, controls.target).normalize();
+        cameraDir.subVectors(cameraDest, camera.position);
+        controlsDir.subVectors(controlsDest, controls.target);
+
+        cameraDir.normalize();
+        controlsDir.normalize();
+
         this.running = true;
     };
 
-
     this.update = (delta) => {
         if (this.running) {
-            const speed = delta * this.movementSpeed;
-            animate(camera.position, cameraDir, speed, speedVector);
-            animate(controls.target, controlsDir, speed, speedVector);
+            cameraDir.subVectors(cameraDest, camera.position);
+            controlsDir.subVectors(controlsDest, controls.target);
+            if (cameraDir.length() > 0.1) {
+                camSpeed = delta * this.movementSpeed * cameraDir.length();
+                cameraDir.normalize();
+                animate(camera.position, cameraDir, camSpeed, speedVector);
+            }
+            if (controlsDir.length() > 0.1) {
+                conSpeed = delta * this.movementSpeed * controlsDir.length();
+                controlsDir.normalize();
+                animate(controls.target, controlsDir, conSpeed, speedVector);
+            }
+            if (cameraDir.length() < 0.1 && controlsDir.length() < 0.1) {
+                this.running = false;
+            }
         }
     };
 
     const cam = new THREE.Vector3(10, 10, 10);
     const ctrl = new THREE.Vector3();
 
-    this.start(cam, ctrl);
+    this.test = () => {
+        this.start(cam, ctrl);
+    };
+
+    this.stop = () => {
+        this.running = false;
+    };
+
+    const guiFolder = window.gui.addFolder('GoTo');
+    guiFolder.add(this, 'test');
+    guiFolder.add(this, 'stop');
 }
 
 function animate(point, direction, speed, speedVector) {
